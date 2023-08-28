172ec35ad2d0bc634e9a73e155e974f5a5fd00ed
Rochel <liuyingfeng57@gmail.com>
2023-08-28 12:41:56 +0800

    首頁介紹區塊

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
+//介紹
+const swiperIntro = new Swiper('.swiperIntro', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+  },
+   breakpoints: {
+     768: {
+       autoplay: {
+         enabled: true,
+       }
+     }
+   },
+ navigation: {
+   nextEl: '.swiper-button-next',
+   prevEl: '.swiper-button-prev',
+ },
+});
+//使用分享
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
